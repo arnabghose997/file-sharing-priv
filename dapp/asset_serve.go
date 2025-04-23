d1c8cb4e3a47fb7b1f05ed38c78f5ef63b2e95c6
Arnab Ghose <arnab.ghose128@gmail.com>
2025-04-23 16:39:38 +0530

    added streaming for upload artifacts

--- dapp/asset_serve.go
+++ dapp/asset_serve.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"path"
@@ -33,22 +34,74 @@ func getClientError(c *gin.Context, errMsg string) {
 	c.JSON(http.StatusBadRequest, gin.H{"status": false, "error": errMsg})
 }
 
+// func handleUploadAsset_UploadArtifacts(c *gin.Context) {
+// 	w := http.ResponseWriter(c.Writer)
+// 	enableCors(&w)
+
+// 	assetFile, err := c.FormFile("asset")
+// 	if err != nil {
+// 		getClientError(c, "Failed to get asset file, asset file is required")
+// 		return
+// 	}
+
+// 	metadata, err := c.FormFile("metadata")
+// 	if err != nil {
+// 		getClientError(c, "Failed to get metadata file, metadata file is required")
+// 		return
+// 	}
+
+// 	uploadDir := "./uploads"
+// 	timeStampDir := fmt.Sprintf("%v", time.Now().Unix())
+// 	uploadDestination := path.Join(uploadDir, timeStampDir)
+// 	if err := os.MkdirAll(uploadDestination, 0755); err != nil {
+// 		getInternalError(c, fmt.Sprintf("failed to create upload directory: %v", err))
+// 		return
+// 	}
+
+// 	assetFilePath := path.Join(uploadDestination, assetFile.Filename)
+// 	if err := c.SaveUploadedFile(assetFile, assetFilePath); err != nil {
+// 		getInternalError(c, fmt.Sprintf("failed to save asset file: %v", err))
+// 		return
+// 	}
+
+// 	metadataFilePath := path.Join(uploadDestination, metadata.Filename)
+// 	if err := c.SaveUploadedFile(metadata, metadataFilePath); err != nil {
+// 		getInternalError(c, fmt.Sprintf("failed to save metadata file: %v", err))
+// 		return
+// 	}
+
+// 	getResult(c, assetFilePath, metadataFilePath)
+// }
+
 func handleUploadAsset_UploadArtifacts(c *gin.Context) {
 	w := http.ResponseWriter(c.Writer)
 	enableCors(&w)
 
-	assetFile, err := c.FormFile("asset")
+	// Get the multipart form
+	form, err := c.MultipartForm()
 	if err != nil {
+		getClientError(c, "Failed to parse multipart form")
+		return
+	}
+	defer form.RemoveAll() // Clean up temporary files
+
+	// Get the asset file
+	assetFiles, assetOk := form.File["asset"]
+	if !assetOk || len(assetFiles) == 0 {
 		getClientError(c, "Failed to get asset file, asset file is required")
 		return
 	}
+	assetFile := assetFiles[0]
 
-	metadata, err := c.FormFile("metadata")
-	if err != nil {
+	// Get the metadata file
+	metadataFiles, metadataOk := form.File["metadata"]
+	if !metadataOk || len(metadataFiles) == 0 {
 		getClientError(c, "Failed to get metadata file, metadata file is required")
 		return
 	}
+	metadata := metadataFiles[0]
 
+	// Create upload directory
 	uploadDir := "./uploads"
 	timeStampDir := fmt.Sprintf("%v", time.Now().Unix())
 	uploadDestination := path.Join(uploadDir, timeStampDir)
@@ -57,14 +110,44 @@ func handleUploadAsset_UploadArtifacts(c *gin.Context) {
 		return
 	}
 
+	// Save asset file
 	assetFilePath := path.Join(uploadDestination, assetFile.Filename)
-	if err := c.SaveUploadedFile(assetFile, assetFilePath); err != nil {
+	assetDest, err := os.Create(assetFilePath)
+	if err != nil {
+		getInternalError(c, fmt.Sprintf("failed to create asset file: %v", err))
+		return
+	}
+	defer assetDest.Close()
+
+	assetSrc, err := assetFile.Open()
+	if err != nil {
+		getInternalError(c, fmt.Sprintf("failed to open asset file: %v", err))
+		return
+	}
+	defer assetSrc.Close()
+
+	if _, err := io.Copy(assetDest, assetSrc); err != nil {
 		getInternalError(c, fmt.Sprintf("failed to save asset file: %v", err))
 		return
 	}
 
+	// Save metadata file
 	metadataFilePath := path.Join(uploadDestination, metadata.Filename)
-	if err := c.SaveUploadedFile(metadata, metadataFilePath); err != nil {
+	metadataDest, err := os.Create(metadataFilePath)
+	if err != nil {
+		getInternalError(c, fmt.Sprintf("failed to create metadata file: %v", err))
+		return
+	}
+	defer metadataDest.Close()
+
+	metadataSrc, err := metadata.Open()
+	if err != nil {
+		getInternalError(c, fmt.Sprintf("failed to open metadata file: %v", err))
+		return
+	}
+	defer metadataSrc.Close()
+
+	if _, err := io.Copy(metadataDest, metadataSrc); err != nil {
 		getInternalError(c, fmt.Sprintf("failed to save metadata file: %v", err))
 		return
 	}
@@ -152,7 +235,7 @@ func handleUploadAsset_GetArtifactInfo(c *gin.Context) {
 func handleDownloadArtifact(c *gin.Context) {
 	w := http.ResponseWriter(c.Writer)
 	enableCors(&w)
-	
+
 	assetCID := c.Param("cid")
 	if assetCID == "" {
 		getClientError(c, "cid is required, it came empty")
